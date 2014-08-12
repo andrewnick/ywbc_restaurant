61d0b2ae4909c126d1e777b789bd3a388ad27f45
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2014-08-12 14:19:29 +1200

    contact and hours information now have improved layout.

--- includes/content-information.php
+++ includes/content-information.php
@@ -1,11 +1,26 @@
 <section class="row content_container">
 	<article class="content_container_img col-xs-12  col-sm-offset-2 col-sm-4">
+
 		<div class="row">
 			<h3 class="col-xs-8 col-xs-offset-2">Hours</h3>
 		</div>
+
 		<div class="row">
-			<h4 class="col-xs-8 col-xs-offset-2 col-sm-12">Mon - Fri: 11am - 11pm</h4>
-			<h4 class="col-xs-8 col-xs-offset-2 col-sm-12">Sat - Sun: 11am - 1am</h4>
+			<div class="col-xs-10 col-xs-offset-2">
+				<h4 class="content_information_title content_information_title-hours">Mon - Fri:</h4>
+				<div class="content_information_details">	
+					<h4>11am - 11pm</h4>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-2">
+				<h4 class="content_information_title content_information_title-hours">Sat - Sun: </h4>
+				<div class="content_information_details">	
+					<h4>11am - 1am</h4>
+				</div>
+			</div>
 		</div>
 	</article>
 
@@ -16,16 +31,22 @@
 		</div>
 
 		<div class="row">
-			<h4 class="col-xs-3 col-xs-offset-2">Phone:</h4>
-			<h4 class="col-xs-7">55-555-5555</h4>
+			<div class="col-xs-10 col-xs-offset-2">
+				<h4 class="content_information_title">Phone: </h4>
+				<div class="content_information_details">	
+					<h4>55-555-5555</h4>
+				</div>
+			</div>
 		</div>
 
 		<div class="row">
-			<h4 class="col-xs-3 col-xs-offset-2">Address: </h4>
-			<div class="col-xs-7">
-				<h4>Kennedy Bush</h4>
-				<h4>Summit Road</h4>
-				<h4>Port Hills</h4>
+			<div class="col-xs-10 col-xs-offset-2">
+				<h4 class="content_information_title">Address: </h4>
+				<div class="content_information_details">	
+					<h4>Kennedy Bush</h4>
+					<h4>Summit Road</h4>
+					<h4>Port Hills</h4>
+				</div>
 			</div>
 		</div>
 
