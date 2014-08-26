f72c4b430e6e606a4e813894b9f16a01a3873da7
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2014-08-26 22:48:42 +1200

    fixed up navigation alignement

--- includes/header.php
+++ includes/header.php
@@ -6,13 +6,13 @@
         <span class="nav_btn_line"></span>
 	</button>
 
-	<div class="col-xs-9 col-sm-4 header_logo_container">
+	<div class="col-xs-9 col-sm-3 header_logo_container">
 		<img src="assets/images/logo_m.png" alt="Logo" class='header_logo'>
 	</div>
 
 
 
-	<nav class='nav_menu col-xs-offset-3 col-xs-9 col-sm-offset-0 col-sm-8 col-md-offset-2 col-md-6'>
+	<nav class='nav_menu col-xs-offset-3 col-xs-9 col-sm-offset-5 col-sm-6'>
 		<div class="row">
 <!-- 			<div class="col-xs-12 col-sm-3 nav_item_container nav_item_container--first">
 				<a class="nav_item" href='#'>Home</a>
